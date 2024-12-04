ba5b9c6ed996641b916ff318a0b2d7ed7babe432
Vem Publicar <127986394+vempublicar@users.noreply.github.com>
2024-12-04 08:25:43 -0300

    ajuste 04.12

--- app/config/path.php
+++ app/config/path.php
@@ -1,9 +1,9 @@
 <?php
-// Ambiente de Desenvolvimento
-$destino = "http://localhost/membros-fepacoc/";
+// Ambiente de Desenvolvimento 3994ed6a
+// $destino = "http://localhost/membros-fepacoc/";
 
 // Ambiente de Produção
-// $destino = "https://members.fepacoc.com.br/";
+$destino = "https://members.fepacoc.com.br/";
 
 define('BASE_URL', $destino);
 ?>
