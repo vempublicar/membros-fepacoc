0a4442ca0913893debea76054b1a08f149d2a2b4
Vem Publicar <127986394+vempublicar@users.noreply.github.com>
2024-12-09 20:20:16 -0300

    zerando

--- app/views/public/verifica-email.php
+++ app/views/public/verifica-email.php
@@ -3,7 +3,7 @@
 <body class="bg-dark">
     <div class="container d-flex justify-content-center align-items-center vh-100">
         <div class="card shadow" style="max-width: 600px; background-color: #242e36; border-radius: 20px;">
-            <div class="card-body text-center p-5">
+            <div class="card-body bg-dark text-center p-5">
                 <img src="vendor/img/verifique-email.png" alt="Verifique seu email" style="width: 200px; height: auto; margin-bottom: 20px;">
                 <h2 class="text-white mb-3">Verifique seu email</h2>
                 <p class="text-white">Foi enviado para seu email a senha de acesso. Por favor, verifique sua caixa de entrada e siga as instruções para acessar sua conta.</p>
