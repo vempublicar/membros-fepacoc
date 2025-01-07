35f1664cb41b66eb0e33a2576ef5f8ddd2523845
Vem Publicar <127986394+vempublicar@users.noreply.github.com>
2025-01-07 16:17:07 -0300

    1.0.02

--- app/views/private/aulas.php
+++ app/views/private/aulas.php
@@ -106,7 +106,7 @@ $videosPagina = array_slice($videos, $inicio, $videosPorPagina);
     <div class="modal-dialog modal-lg">
         <div class="modal-content">
             <div class="modal-header">
-                <h5 class="modal-title" id="videoModalLabel">Vídeo</h5>
+                <h5 class="modal-title" id="videoModalLabel">Vídeo Aulas</h5>
                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
             <div class="modal-body">
