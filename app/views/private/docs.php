0ecf4f244935081342a4627ac58cb2e720a74147
Vem Publicar <127986394+vempublicar@users.noreply.github.com>
2025-01-08 13:42:15 -0300

    1

--- app/views/private/docs.php
+++ app/views/private/docs.php
@@ -1,6 +1,6 @@
 <?php
 // Caminho base dos arquivos
-$basePath = 'vendor/img/produto/play/';
+$basePath = '../../vendor/img/produto/play/';
 
 // Obtenha o nome do arquivo solicitado (parâmetro GET)
 $file = $_GET['file'] ?? null;
@@ -8,6 +8,7 @@ $file = $_GET['file'] ?? null;
 if ($file) {
     // Evite que acessem arquivos fora da pasta especificada
     $realPath = realpath($basePath . $file);
+    echo 'teste';
 print_r($realPath);
     if ($realPath && strpos($realPath, $basePath) === 0 && file_exists($realPath)) {
         // Determine o tipo MIME do arquivo
@@ -26,4 +27,4 @@ print_r($realPath);
 
 // Caso o arquivo não seja encontrado ou não seja permitido
 http_response_code(404);
-echo "Arquivo não encontrado ou acesso não permitido neste momento.".$_GET['file'];
+echo "Arquivo não encontrado ou acesso não permitido neste momento.".$file;
