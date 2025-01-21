e37037e1f4c16e352e724980c798f88601f75384
Vem Publicar <127986394+vempublicar@users.noreply.github.com>
2025-01-20 23:06:13 -0300

    1

--- app/views/public/log-webhook.php
+++ app/views/public/log-webhook.php
@@ -1,24 +1,4 @@
 <?php
 curl -X POST -H "Content-Type: application/json" -d '{"id": "test_id", "type": "customer.created"}' https://members.fepacoc.com.br/webhook
 
-// Caminho para o arquivo de log
-$logFilePath = 'app/functions/push/webhook_log.txt';
-
-// Verifica se o arquivo de log existe
-if (file_exists($logFilePath)) {
-    // Abre o arquivo de log
-    $file = fopen($logFilePath, "r");
-
-    // Lê o conteúdo do arquivo de log
-    echo "<h1>Conteúdo do Log de Webhook:</h1>";
-    echo "<pre>";
-    while (($line = fgets($file)) !== false) {
-        echo htmlspecialchars($line) . "<br>";
-    }
-    echo "</pre>";
-
-    // Fecha o arquivo
-    fclose($file);
-} else {
-    echo "O arquivo de log não foi encontrado.";
-}
+?>
