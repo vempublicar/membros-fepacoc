259ae176e3350e47b02111e6100d6ce8cdd84feb
Vem Publicar <127986394+vempublicar@users.noreply.github.com>
2025-01-20 23:40:31 -0300

    1

--- app/views/public/webhook.php
+++ app/views/public/webhook.php
@@ -9,6 +9,7 @@ $data = file_get_contents("php://input");
 logData("Received: " . $data . "\n");
 
 $json = json_decode($data, true);
+logData('Json: ' . print_r($json, true) . "\n");
 $result = handlePagarMeWebhook($json);  // Ajuste para chamar a função correta
 logData('Resposta: ' . print_r($result, true) . "\n");
 
