b3cc02160160a39bf8a26eb91b6bcea7d4180379
Vem Publicar <127986394+vempublicar@users.noreply.github.com>
2025-01-31 16:54:35 -0300

    1

--- app/views/adm/painel-adm.php
+++ app/views/adm/painel-adm.php
@@ -18,6 +18,7 @@ $ferramentas = fetchFerramentas();
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css" rel="stylesheet">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" rel="stylesheet">
     <link href="https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css" rel="stylesheet">
     <link rel="stylesheet" href="vendor/css/painel-style.css">
 
