157a52945578a310c323a3ec735d284b105a77e5
Vem Publicar <127986394+vempublicar@users.noreply.github.com>
2025-01-31 10:03:56 -0300

    1

--- app/views/adm/painel-adm.php
+++ app/views/adm/painel-adm.php
@@ -102,8 +102,9 @@ include_once "app/views/parts/head.php"; ?>
         </div>
     </div>
 
-    <!-- Bootstrap JavaScript -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js"
+    integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+"
+    crossorigin="anonymous"></script>
 
     <script>
         document.addEventListener('DOMContentLoaded', function () {
