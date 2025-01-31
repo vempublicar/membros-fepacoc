310fa29473e98af3473b97fb7a67fdd214173cf3
Vem Publicar <127986394+vempublicar@users.noreply.github.com>
2025-01-31 20:24:31 -0300

    1

--- app/views/adm/painel-adm.php
+++ app/views/adm/painel-adm.php
@@ -48,8 +48,8 @@ $ferramentas = fetchFerramentas();
 
     <div class="d-flex">
         <div id="sidebar" class="d-flex flex-column flex-shrink-0 p-3 bg-dark text-light sidebar d-none d-md-flex" style="height: 100vh;">
-            <a href="#" class="d-flex align-items-center mb-3 mb-md-0 text-light text-decoration-none">
-                <span class="fs-4">ADM</span>
+            <a href="painel" class="d-flex align-items-center mb-3 mb-md-0 text-light text-decoration-none">
+                <span class="fs-4">Painel Principal</span>
             </a>
             <hr class="border-secondary">
             <ul class="nav nav-pills flex-column mb-auto">
