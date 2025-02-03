dd4b86aeef209d1c45a2522470df9525f2d5b5f9
Vem Publicar <127986394+vempublicar@users.noreply.github.com>
2025-02-03 10:56:14 -0300

    1

--- app/views/parts/header.php
+++ app/views/parts/header.php
@@ -1,10 +1,10 @@
 <header id="top" class="position-sticky top-0 start-0" style="z-index: 999;">
     <nav class="navbar navbar-expand-lg bg-white fixed-top shadow-sm">
         <div class="container-fluid d-flex justify-content-between align-items-center">
-            <!-- Logo e Texto "Membros" -->
+            <!-- Logo -->
             <div class="d-flex align-items-center">
                 <a class="navbar-brand d-flex align-items-center" href="painel">
-                    <img src="vendor/img/logo_escuro.png" class="img-fluid" style="height: 26px;" id="logo" alt="Logo">
+                    <img src="vendor/img/logo_escuro.png" class="img-fluid" style="height: 22px;" id="logo" alt="Logo">
                 </a>
             </div>
 
@@ -13,9 +13,9 @@
                 <span class="navbar-toggler-icon"></span>
             </button>
 
-            <!-- Links do Menu -->
+            <!-- Links do Menu Centralizados -->
             <div class="collapse navbar-collapse justify-content-center" id="navbarNav">
-                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <ul class="navbar-nav text-center">
                     <li class="nav-item">
                         <a class="nav-link" href="painel">Painel</a>
                     </li>
@@ -34,12 +34,8 @@
                 </ul>
             </div>
 
+            <!-- Perfil e Configurações -->
             <div class="d-flex align-items-center gap-3">
-                <button id="themeToggle" class="btn btn-outline-secondary">
-                    <i class="fas fa-sun"></i>
-                </button>
-
-                <!-- Foto de Perfil com Dropdown -->
                 <div class="dropdown">
                     <a href="#" id="profileDropdown" class="d-flex align-items-center text-decoration-none" data-bs-toggle="dropdown" aria-expanded="false">
                         <img src="vendor/images/post-thumb-1.jpg" alt="User Profile" class="rounded-circle" width="40" height="40">
@@ -47,7 +43,10 @@
                     <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="profileDropdown">
                         <li><a class="dropdown-item text-black" href="painel">Início</a></li>
                         <li><a class="dropdown-item text-black" href="minha-conta">Conta</a></li>
-                        <!-- <li><a class="dropdown-item" href="minha-assinatura">Assinatura</a></li> -->
+                        <li><hr class="dropdown-divider"></li>
+                        <li>
+                            <a class="dropdown-item text-black" href="#" id="themeToggle">Tema: <span id="themeLabel">Light</span></a>
+                        </li>
                         <li><hr class="dropdown-divider"></li>
                         <li><a class="dropdown-item text-black" href="logout">Logout</a></li>
                     </ul>
