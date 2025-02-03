7097267159ccf6afb4c1d7250466100c3eb7e965
Vem Publicar <127986394+vempublicar@users.noreply.github.com>
2025-02-03 12:28:14 -0300

    1

--- app/views/parts/header.php
+++ app/views/parts/header.php
@@ -9,7 +9,7 @@
             </div>
 
             <!-- Botão de Toggle para Mobile -->
-            <button class="navbar-toggler text-white" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <button class="navbar-toggler btn btn-white" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon "></span>
             </button>
 
