5150497c3eaf9922bd7f70cd4bff9f653955f387
Vem Publicar <127986394+vempublicar@users.noreply.github.com>
2025-02-03 09:16:57 -0300

    1

--- app/views/private/videos.php
+++ app/views/private/videos.php
@@ -94,7 +94,7 @@ $videosFiltrados = array_filter($videos, function ($video) use ($assuntoSelecion
         <h5 class="offcanvas-title fw-bold" id="videoOffcanvasLabel">Detalhes do Vídeo</h5>
         <button type="button" class="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Fechar"></button>
     </div>
-    <div class="offcanvas-body">
+    <div class="offcanvas-body dark-mode">
         <!-- Player do vídeo -->
         <div class="ratio ratio-16x9 mb-3" id="videoContainer">
             <iframe id="videoPlayerIframe" class="d-none" src="" frameborder="0" allowfullscreen></iframe>
